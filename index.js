c8d76d5d07449d1fd74f9a3a270b1caba10ec862
Carlos Sebastián Rivera Espín <95983843+csrivera3@users.noreply.github.com>
2025-08-07 08:28:37 -0500

    Cambio en el index.js, Puerto

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-//Simular endpoint de respuesta
+// Simular endpoint de respuesta
 app.get('/', (req, res) => {
-    res.send('Integracion continua funcionando correctamente');
-    });
-//Iniciar servidor
+  res.send('Integración continua funcionando correctamente');
+});
+
+// Iniciar servidor
 app.listen(PORT, () => {
-    console.log(`Servidor ejecutandose en el puerto ${PORT}`);
+  console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
